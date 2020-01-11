e328315ccca72d2d0c48c22129ff8842a77e3312
MinGRn <MinGRn97@gmail.com>
2020-01-12 00:35:05 +0800

    增加动画菜单测试

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -34,6 +34,7 @@
                 menuItemList: [{
                     id: 1,
                     label: '系统设置',
+                    animated: 'fadeIn',
                     isLeaf: false,
                     icon: {
                         isElement: true,
@@ -42,16 +43,58 @@
                     children: [{
                         id: 10,
                         label: 'Git',
+                        animated: 'fadeIn',
                         isLeaf: true,
                         path: '/git',
                         icon: {
                             isElement: false,
                             class: 'fa-git'
                         }
+                    },{
+                        id: 11,
+                        label: '平台',
+                        animated: 'rollIn',
+                        isLeaf: false,
+                        icon: {
+                            isElement: true,
+                            class: 'el-icon-s-platform'
+                        },
+                        children: [{
+                            id: 110,
+                            label: 'Windows',
+                            animated: 'fadeIn',
+                            isLeaf: true,
+                            path: '/windows',
+                            icon: {
+                                isElement: false,
+                                class: 'fa-windows'
+                            }
+                        },{
+                            id: 111,
+                            label: 'Linux',
+                            animated: 'fadeIn',
+                            isLeaf: true,
+                            path: '/linux',
+                            icon: {
+                                isElement: false,
+                                class: 'fa-linux'
+                            }
+                        },{
+                            id: 112,
+                            label: 'Mac OS',
+                            animated: 'fadeIn',
+                            isLeaf: true,
+                            path: '/macOs',
+                            icon: {
+                                isElement: false,
+                                class: 'fa-apple'
+                            }
+                        }]
                     }]
                 },{
                     id: 2,
                     label: '你好, 世界',
+                    animated: 'tada',
                     isLeaf: true,
                     path: '/helloWorld',
                     icon: {
