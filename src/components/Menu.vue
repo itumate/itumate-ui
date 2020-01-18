bc51b82ba673502d03ffa6e02a18841806d7fd2a
MinGRn <MinGRn97@gmail.com>
2020-01-18 18:08:40 +0800

    设置全局组件, 菜单页面修改
    设置SliderToggle和PaginationHelper全局组件;
    新增 axios、bootstrap、bootstrap-vue、echarts依赖;

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -34,18 +34,147 @@
             <el-form-item label="日期范围" prop="field103">
               <el-date-picker type="daterange" v-model="formData.field103" format="yyyy-MM-dd"
                               value-format="yyyy-MM-dd" :style="{width: '100%'}" start-placeholder="开始日期" end-placeholder="结束日期"
-                              range-separator="至" clearable></el-date-picker>
+                              range-separator="至" clearable>
+              </el-date-picker>
             </el-form-item>
           </el-col>
-          <el-col :span="24">
-            <el-form-item size="large">
+
+          <slider-toggle>
+            <el-col :span="12" v-show="doesShowMoreCondition">
+              <el-form-item label="手机号" prop="mobile">
+                <el-input v-model="formData.mobile" placeholder="请输入手机号" :maxlength="11" show-word-limit clearable
+                          prefix-icon='el-icon-mobile' :style="{width: '100%'}"></el-input>
+              </el-form-item>
+            </el-col>
+          </slider-toggle>
+          <slider-toggle>
+            <el-col :span="12" v-show="doesShowMoreCondition">
+              <el-form-item label="手机号" prop="mobile">
+                <el-input v-model="formData.mobile" placeholder="请输入手机号" :maxlength="11" show-word-limit clearable
+                          prefix-icon='el-icon-mobile' :style="{width: '100%'}"></el-input>
+              </el-form-item>
+            </el-col>
+          </slider-toggle>
+          <slider-toggle>
+            <el-col :span="12" v-show="doesShowMoreCondition">
+              <el-form-item label="手机号" prop="mobile">
+                <el-input v-model="formData.mobile" placeholder="请输入手机号" :maxlength="11" show-word-limit clearable
+                          prefix-icon='el-icon-mobile' :style="{width: '100%'}"></el-input>
+              </el-form-item>
+            </el-col>
+          </slider-toggle>
+          <slider-toggle>
+            <el-col :span="12" v-show="doesShowMoreCondition">
+              <el-form-item label="手机号" prop="mobile">
+                <el-input v-model="formData.mobile" placeholder="请输入手机号" :maxlength="11" show-word-limit clearable
+                          prefix-icon='el-icon-mobile' :style="{width: '100%'}"></el-input>
+              </el-form-item>
+            </el-col>
+          </slider-toggle>
+          <slider-toggle>
+            <el-col :span="12" v-show="doesShowMoreCondition">
+              <el-form-item label="手机号" prop="mobile">
+                <el-input v-model="formData.mobile" placeholder="请输入手机号" :maxlength="11" show-word-limit clearable
+                          prefix-icon='el-icon-mobile' :style="{width: '100%'}"></el-input>
+              </el-form-item>
+            </el-col>
+          </slider-toggle>
+          <slider-toggle>
+            <el-col :span="12" v-show="doesShowMoreCondition">
+              <el-form-item label="手机号" prop="mobile">
+                <el-input v-model="formData.mobile" placeholder="请输入手机号" :maxlength="11" show-word-limit clearable
+                          prefix-icon='el-icon-mobile' :style="{width: '100%'}"></el-input>
+              </el-form-item>
+            </el-col>
+          </slider-toggle>
+          <slider-toggle>
+            <el-col :span="12" v-show="doesShowMoreCondition">
+              <el-form-item label="手机号" prop="mobile">
+                <el-input v-model="formData.mobile" placeholder="请输入手机号" :maxlength="11" show-word-limit clearable
+                          prefix-icon='el-icon-mobile' :style="{width: '100%'}"></el-input>
+              </el-form-item>
+            </el-col>
+          </slider-toggle>
+          <slider-toggle>
+            <el-col :span="12" v-show="doesShowMoreCondition">
+              <el-form-item label="手机号" prop="mobile">
+                <el-input v-model="formData.mobile" placeholder="请输入手机号" :maxlength="11" show-word-limit clearable
+                          prefix-icon='el-icon-mobile' :style="{width: '100%'}"></el-input>
+              </el-form-item>
+            </el-col>
+          </slider-toggle>
+
+          <el-col class="more-condition">
+            <el-divider><span @click="showMoreCondition">{{ showMoreConditionText }}<i class="bars" :class="showMoreConditionSuffixClass"></i></span></el-divider>
+          </el-col>
+
+          <el-col class="query-btn">
+            <el-form-item size="large" style="float: left;">
               <el-button type="primary" @click="submitForm">提交</el-button>
               <el-button @click="resetForm">重置</el-button>
             </el-form-item>
           </el-col>
+
         </el-form>
+
       </el-row>
 
+      <el-table
+        stripe
+        border
+        header-row-class-name="header-row"
+        :data="tableData"
+        :height="screen.innerHeight"
+        style="width: 100%">
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址"
+          width="300">
+        </el-table-column>
+        <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+        </el-table-column>
+        <el-table-column
+          prop="address"
+          label="地址"
+          width="300">
+        </el-table-column>
+
+        <el-table-column
+          fixed="right"
+          label="操作"
+          width="180">
+          <template slot-scope="scope">
+            <el-button type="text" size="small">查看</el-button>
+            <el-button type="text" size="small">编辑</el-button>
+            <el-button type="text" size="small">删除</el-button>
+          </template>
+        </el-table-column>
+
+      </el-table>
+
+      <pagination-helper
+        :background="true"
+        :total="1000"
+        :pageSize="10"
+        :showAllLayout="true" />
     </el-main>
   </el-container>
 </template>
@@ -54,17 +183,18 @@
 
     import Tree from "./tree/Tree";
 
-    import HelloWorld from "./HelloWorld";
+    import MenuData from "../../demo/MenuData";
 
     export default {
         name: 'Menu',
         components: {
-            Tree,
-            HelloWorld
+            Tree
         },
         methods: {
+            handleClick(row){
+                console.log(row)
+            },
             loadTreeNode(node, resolve) {
-                // let id = Math.random();
                 let id = 1;
                 setTimeout(() => {
                     const data = [{
@@ -130,44 +260,43 @@
             },
             submitForm() {
                 this.$refs['elForm'].validate(valid => {
-                    if (!valid) return
-                    // TODO 提交表单
+                    if (!valid) return false;
                 })
             },
             resetForm() {
                 this.$refs['elForm'].resetFields()
             },
-            getField102Options() {
-                // TODO 发起请求获取数据
-                this.field102Options
+            showMoreCondition(){
+                this.doesShowMoreCondition = !this.doesShowMoreCondition;
+                this.showMoreConditionText = this.doesShowMoreCondition ? '收起' : '更多条件';
+                this.showMoreConditionSuffixClass = this.doesShowMoreCondition ? 'fa fa-angle-up' : 'fa fa-angle-down';
+                console.log(this.doesShowMoreCondition, this.showMoreConditionText)
+            },
+            resizeScreen() {
+                let unit = (window.innerHeight - 100) / 48 - 2;
+                this.screen.innerHeight = unit * 48;
             }
+        },
+        created(){
+            this.resizeScreen();
+        },
+        mounted() {
+            window.onresize = () => {
+                this.resizeScreen();
+            };
         },
         data(){
             return{
-                treeNodes: [{
-                    id: 1,
-                    isLeaf: false,
-                    label: '系统设置',
-                    icon: {
-                        class: 'fa-cog'
-                    },
-                    children: [{
-                        id: 10,
-                        isLeaf: false,
-                        label: '菜单管理',
-                        icon: {
-                            class: 'fa-align-justify'
-                        }
-                    }, {
-                        id: 11,
-                        isLeaf: false,
-                        label: '用户管理',
-                        icon: {
-                            class: 'fa-user'
-                        }
-                    }]
-                }],
-                /* 测试 */
+                screen: {
+                    innerHeight: 0,
+                },
+                doesShowMoreCondition: false,
+                showMoreConditionText: '更多条件',
+                showMoreConditionSuffixClass: 'fa fa-angle-down',
+
+                /* 以下为测试数据 */
+                treeNodes: MenuData.treeNode,
+                tableData: MenuData.tableData,
                 formData: {
                     field101: undefined,
                     mobile: '',
@@ -204,7 +333,7 @@
                 field102Options: [],
                 field102Props: {
                     "multiple": false
-                },
+                }
             }
         }
     }
--- src/main.js
+++ src/main.js
@@ -7,16 +7,19 @@ import './assets/css/app.css';
 import './assets/css/theme/index.css';
 import './assets/css/webkit.scrollbar.css';
 
-// waves 效果
-// import 'materialize-css/dist/js/materialize.min';
-// import 'materialize-css/dist/css/materialize.min.css';
-
 // font-awesome
 import 'font-awesome/css/font-awesome.min.css';
 import 'animate.css';
 
 import App from './App';
 import router from './router';
+
+// Global Component
+import SliderToggle from "@/components/global/SliderToggle";
+import PaginationHelper from "@/components/global/PaginationHelper";
+
+Vue.component('SliderToggle', SliderToggle);
+Vue.component('PaginationHelper', PaginationHelper);
 
 // filter
 import {currency} from './filter/currency';
