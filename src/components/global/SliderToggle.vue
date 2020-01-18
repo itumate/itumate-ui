eac9a81d55811f3a39e60cd659d1803de364dfa5
MinGRn <MinGRn97@gmail.com>
2020-01-18 18:03:33 +0800

    增加 SliderToggle 组件

---
+++ src/components/global/SliderToggle.vue
@@ -0,0 +1,88 @@
+<!--
+    实现 jQuery SliderToggle 效果.
+
+    How To Use ?
+
+    import SliderToggle from '@path';
+
+    <slider-toggle>
+        <div v-show="active = !active">
+
+        </div>
+    </slider-toggle>
+-->
+<script>
+    export default {
+        name: 'SliderToggle',
+        functional: true,
+        render(h, {children}) {
+            const data = {
+                on: Transition
+            };
+            return h('transition', data, children)
+        }
+    }
+
+    const elTransition = '0.3s height ease-in-out, 0.3s padding-top ease-in-out, 0.3s padding-bottom ease-in-out'
+    const Transition = {
+        'before-enter'(el) {
+            el.style.transition = elTransition;
+            if (!el.dataset) el.dataset = {};
+
+            el.dataset.oldPaddingTop = el.style.paddingTop;
+            el.dataset.oldPaddingBottom = el.style.paddingBottom;
+
+            el.style.height = 0;
+            el.style.paddingTop = 0;
+            el.style.paddingBottom = 0
+        },
+
+        'enter'(el) {
+            el.dataset.oldOverflow = el.style.overflow;
+            if (el.scrollHeight !== 0) {
+                el.style.height = el.scrollHeight + 'px';
+                el.style.paddingTop = el.dataset.oldPaddingTop;
+                el.style.paddingBottom = el.dataset.oldPaddingBottom;
+            } else {
+                el.style.height = '';
+                el.style.paddingTop = el.dataset.oldPaddingTop;
+                el.style.paddingBottom = el.dataset.oldPaddingBottom
+            }
+
+            el.style.overflow = 'hidden'
+        },
+
+        'after-enter'(el) {
+            el.style.transition = '';
+            el.style.height = '';
+            el.style.overflow = el.dataset.oldOverflow
+        },
+
+        'before-leave'(el) {
+            if (!el.dataset) el.dataset = {};
+            el.dataset.oldPaddingTop = el.style.paddingTop;
+            el.dataset.oldPaddingBottom = el.style.paddingBottom;
+            el.dataset.oldOverflow = el.style.overflow;
+
+            el.style.height = el.scrollHeight + 'px';
+            el.style.overflow = 'hidden'
+        },
+
+        'leave'(el) {
+            if (el.scrollHeight !== 0) {
+                el.style.transition = elTransition;
+                el.style.height = 0;
+                el.style.paddingTop = 0;
+                el.style.paddingBottom = 0
+            }
+        },
+
+        'after-leave'(el) {
+            el.style.transition = '';
+            el.style.height = '';
+            el.style.overflow = el.dataset.oldOverflow;
+            el.style.paddingTop = el.dataset.oldPaddingTop;
+            el.style.paddingBottom = el.dataset.oldPaddingBottom;
+        }
+    };
+</script>
