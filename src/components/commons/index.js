81b8cfc0e5774812fc024d5c2e9ec41ad06ae84c
MinGRn <MinGRn97@gmail.com>
2020-02-21 14:42:27 +0800

    新增 commons 导出 index

---
+++ src/components/commons/index.js
@@ -0,0 +1,11 @@
+import SliderToggle from "./SliderToggle";
+import PaginationHelper from "./PaginationHelper";
+import PageBody from "./PageBody";
+import PageHeader from "./PageHeader";
+
+export default (Vue) => {
+  Vue.component('SliderToggle', SliderToggle);
+  Vue.component('PaginationHelper', PaginationHelper);
+  Vue.component('PageBody', PageBody);
+  Vue.component('PageHeader', PageHeader);
+}
